d91904b167a45066943e9f2179c9ed026cf2a1d8
JuanitaCeci <juanitaceci0624@gmail.com>
2021-08-11 19:34:01 -0400

    first commit

---
+++ src/GitFinalClassPack/GitClass03.java
@@ -0,0 +1,13 @@
+package GitFinalClassPack;
+
+public class GitClass03 {
+
+	public static void main(String[] args) {
+
+System.out.println();
+		System.out.println(" This is the 1st commit file");
+		
+		// some notes
+	}
+
+}
